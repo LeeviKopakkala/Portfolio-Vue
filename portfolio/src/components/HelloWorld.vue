98d3c2da4f554db311d9bdeadb624791f71de139
LeeviKopakkala <leevi.kopakkala@hotmail.com>
2019-03-19 22:17:29 +0200

    Add SASS

--- portfolio/src/components/HelloWorld.vue
+++ portfolio/src/components/HelloWorld.vue
@@ -40,7 +40,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
 h3 {
   margin: 40px 0 0;
 }
@@ -53,6 +54,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: $primary;
 }
 </style>
--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+// Import Components
+import HelloWorld from './components/HelloWorld'
+
+// Routes
+const routes = [
+	{ path: '/', component: HelloWorld },
+	{ path: '/test', component: HelloWorld },
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
