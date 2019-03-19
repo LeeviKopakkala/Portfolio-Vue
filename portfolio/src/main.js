a9208b185a61a2ac6197ff48a60a07aee8d2397f
LeeviKopakkala <leevi.kopakkala@hotmail.com>
2019-03-19 22:20:45 +0200

    Add Boostrap

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+import 'bootstrap'; import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
